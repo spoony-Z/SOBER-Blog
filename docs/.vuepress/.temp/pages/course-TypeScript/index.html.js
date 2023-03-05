2538a7d9771979656fdf1e5c1df4141cc938eeec
杨胜军 <shenjun_y@yeah.net>
2023-03-05 10:20:01 +0800

    转换类型

--- docs/.vuepress/.temp/pages/course-TypeScript/index.html.js
+++ docs/.vuepress/.temp/pages/course-TypeScript/index.html.js
@@ -1,4 +1,4 @@
-export const data = JSON.parse("{\"key\":\"v-b94127de\",\"path\":\"/course-TypeScript/\",\"title\":\"TypeScript 学习笔记\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"course-TypeScript/README.md\"}")
+export const data = JSON.parse("{\"key\":\"v-b94127de\",\"path\":\"/course-TypeScript/\",\"title\":\"TypeScript 学习笔记\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1677926209000,\"contributors\":[{\"name\":\"spoony_Z\",\"email\":\"shenjun_y@yeah.net\",\"commits\":1}]},\"filePathRelative\":\"course-TypeScript/README.md\"}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
--- docs/.vuepress/.temp/pages/course-recat/index.html.js
+++ docs/.vuepress/.temp/pages/course-recat/index.html.js
@@ -1,4 +1,4 @@
-export const data = JSON.parse("{\"key\":\"v-67a6e765\",\"path\":\"/course-recat/\",\"title\":\"React 学习笔记\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"course-recat/README.md\"}")
+export const data = JSON.parse("{\"key\":\"v-67a6e765\",\"path\":\"/course-recat/\",\"title\":\"React 学习笔记\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1677926209000,\"contributors\":[{\"name\":\"spoony_Z\",\"email\":\"shenjun_y@yeah.net\",\"commits\":1}]},\"filePathRelative\":\"course-recat/README.md\"}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
