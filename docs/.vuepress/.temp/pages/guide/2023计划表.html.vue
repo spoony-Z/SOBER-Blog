62e93f342a9d2353146212888491533cf93acef5
spoony_Z <shenjun_y@yeah.net>
2023-03-06 09:10:36 +0800

    提交计划

--- docs/.vuepress/.temp/pages/guide/2023计划表.html.vue
+++ docs/.vuepress/.temp/pages/guide/2023计划表.html.vue
@@ -5,10 +5,16 @@
 <pre><code>vue3 + vite + TS 学习完成
 </code></pre>
 <h2 id="_5月-6月" tabindex="-1"><a class="header-anchor" href="#_5月-6月" aria-hidden="true">#</a> 5月 --- 6月</h2>
-<pre><code>JavaScript 入门
+<pre><code>css、JavaScript 复习
+</code></pre>
+<h2 id="_6月-7月" tabindex="-1"><a class="header-anchor" href="#_6月-7月" aria-hidden="true">#</a> 6月 --- 7月</h2>
+<pre><code>three.js
 </code></pre>
 <h2 id="_7月-8月" tabindex="-1"><a class="header-anchor" href="#_7月-8月" aria-hidden="true">#</a> 7月 --- 8月</h2>
-<pre><code>python 
+<pre><code>react
+</code></pre>
+<h2 id="_8月-9月" tabindex="-1"><a class="header-anchor" href="#_8月-9月" aria-hidden="true">#</a> 8月 --- 9月</h2>
+<pre><code>python
 </code></pre>
 </div></template>
 
