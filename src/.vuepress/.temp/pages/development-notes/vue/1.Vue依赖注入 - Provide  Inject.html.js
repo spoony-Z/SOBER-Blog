67d9906fbf46106a0c521c33cb27dcc21f061939
spoony_Z <shenjun_y@yeah.net>
2023-04-10 16:40:16 +0800

    LLL

---
+++ src/.vuepress/.temp/pages/development-notes/vue/1.Vue依赖注入 - Provide  Inject.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-1a27c043\",\"path\":\"/development-notes/vue/1.Vue%E4%BE%9D%E8%B5%96%E6%B3%A8%E5%85%A5%20-%20Provide%20%20Inject.html\",\"title\":\"Vue 依赖注入 - Provide / Inject(重点)\",\"lang\":\"zh-CN\",\"frontmatter\":{\"description\":\"Vue 依赖注入 - Provide / Inject(重点)\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://mister-hope.github.io/SOBER-Blog/development-notes/vue/1.Vue%E4%BE%9D%E8%B5%96%E6%B3%A8%E5%85%A5%20-%20Provide%20%20Inject.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"SOBER\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Vue 依赖注入 - Provide / Inject(重点)\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"Vue 依赖注入 - Provide / Inject(重点)\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"article:author\",\"content\":\"SOBER\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"Article\\\",\\\"headline\\\":\\\"Vue 依赖注入 - Provide / Inject(重点)\\\",\\\"image\\\":[\\\"\\\"],\\\"dateModified\\\":null,\\\"author\\\":[{\\\"@type\\\":\\\"Person\\\",\\\"name\\\":\\\"SOBER\\\",\\\"url\\\":\\\"https://github.com/spoony-Z\\\"}]}\"]]},\"headers\":[],\"readingTime\":{\"minutes\":0.53,\"words\":159},\"filePathRelative\":\"development-notes/vue/1.Vue依赖注入 - Provide  Inject.md\",\"excerpt\":\"<h1> Vue 依赖注入 - Provide / Inject(重点)</h1>\\n\",\"autoDesc\":true}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
---
+++ src/.vuepress/.temp/pages/zh/index.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-2d0ad528\",\"path\":\"/zh/\",\"title\":\"主要功能与配置演示\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"主要功能与配置演示\",\"index\":false,\"icon\":\"discover\",\"category\":[\"使用指南\"],\"description\":\"目录 页面展示 页面配置 个人信息\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://mister-hope.github.io/SOBER-Blog/zh/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"SOBER\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"主要功能与配置演示\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"目录 页面展示 页面配置 个人信息\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"article:author\",\"content\":\"SOBER\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"Article\\\",\\\"headline\\\":\\\"主要功能与配置演示\\\",\\\"image\\\":[\\\"\\\"],\\\"dateModified\\\":null,\\\"author\\\":[{\\\"@type\\\":\\\"Person\\\",\\\"name\\\":\\\"SOBER\\\",\\\"url\\\":\\\"https://github.com/spoony-Z\\\"}]}\"]]},\"headers\":[{\"level\":2,\"title\":\"目录\",\"slug\":\"目录\",\"link\":\"#目录\",\"children\":[]}],\"readingTime\":{\"minutes\":0.12,\"words\":36},\"filePathRelative\":\"zh/README.md\",\"excerpt\":\"<h2> 目录</h2>\\n<ul>\\n<li>\\n<p><a href=\\\"/SOBER-Blog/zh/page.html\\\" target=\\\"blank\\\">页面展示</a></p>\\n</li>\\n<li>\\n<p><a href=\\\"/SOBER-Blog/zh/slides.html\\\" target=\\\"blank\\\">页面配置</a></p>\\n</li>\\n<li>\\n<p><a href=\\\"/SOBER-Blog/zh/intro.html\\\" target=\\\"blank\\\">个人信息</a></p>\\n</li>\\n</ul>\\n\",\"autoDesc\":true}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
